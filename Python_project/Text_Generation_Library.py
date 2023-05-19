ad29e2b9ed1f280bd3973c8dd268f566a51a32a3
Leonardo Rocca <leo.rocca03@gmail.com>
2023-05-19 15:39:29 +0200

    Generation library complete.

--- Python_project/Text_Generation_Library.py
+++ Python_project/Text_Generation_Library.py
@@ -1,11 +1,10 @@
-from geopy.geocoders import Nominatim
-from langchain import PromptTemplate
-from langchain.llms import OpenAI
-from langchain import LLMChain
-import random as rd
-import requests
-import pprint
 import os
+
+import requests
+from geopy.geocoders import Nominatim
+from langchain import LLMChain
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
 
 
 def setup_api_openai(API_key: str):
@@ -13,8 +12,8 @@ def setup_api_openai(API_key: str):
     os.environ["OPENAI_API_KEY"] = API_key
 
     # Define the language model and the temperature
-    llm = OpenAI(
-        model_name="text-davinci-002",
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
         temperature=1
     )
 
@@ -30,9 +29,50 @@ def setup_weather(Country: str, City: str):
 
     return weather_data['current_weather']
 
-def generate_prompt(context: dict, llm):
-    ...
+
+def generate_prompt(context, llm, infos: tuple):
+    sex, age, mood, flight_duration, time_before_departure, airline_company, products = infos
+
+    # Define the prompt template
+    template = """
+    Write a targeted 1 short sentence long advertisement knowing the following information about the person:
+    {sex}, {age} years old, who is currently feeling {mood}.
+    You should keep in mind that our target is a person taking a {flight_duration}-hours flight, has {time_before_departure} 
+    hours left before departure, and flies with {airline_company} so keep it in mind to target the pricing accordingly. 
+    Capture their attention and emphasize how this {products} knowing that the meteo in the city the person is currently in is {context}.
+    The output should exclude any personal information about the person and should adress the target personally,
+    (speaking to him like a friend), and the him why he should be interested to the ad.
+    """
+
+    prompt = PromptTemplate(
+        template=template,
+        input_variables=['sex', 'age', 'mood', 'flight_duration', 'time_before_departure', 'airline_company',
+                         'products', 'context'],
+    )
+
+    llm_chain = LLMChain(
+        prompt=prompt,
+        llm=llm,
+        verbose=True
+    )
+
+    results = llm_chain.run(
+        sex=sex,
+        age=age,
+        mood=mood,
+        flight_duration=flight_duration,
+        time_before_departure=time_before_departure,
+        airline_company=airline_company,
+        products=products,
+        context=context
+    )
+
+    return results
 
 
 if __name__ == "__main__":
-    pprint.pprint(setup_weather("Italy", "Rome"))
+    llm = setup_api_openai('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')
+    weather_data = setup_weather('Rome', 'Italy')
+    ad = generate_prompt(weather_data, llm, ['Male', '30', 'Happy', '2', '2', 'Emirates', 'coffee'])
+
+    print(ad)
