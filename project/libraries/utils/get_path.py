81ef47ee783b5c85eb39b2205f81a677cf4db17b
AlessandroKuz <alessandrokuz@gmail.com>
2023-05-24 15:00:06 +0200

    update file structure

--- project/libraries/utils/get_path.py
+++ project/libraries/utils/get_path.py
@@ -4,6 +4,6 @@ import os
 def get_path():
     path = os.path.dirname(os.path.abspath(__file__))
     # going up to the parent directory (main directory)
-    path = os.path.dirname(path)
+    path = os.path.dirname(os.path.dirname(path))
 
     return path
